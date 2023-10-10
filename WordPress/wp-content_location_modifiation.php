21092c7b667f8b520fcc2c0f40c607614447cec9
Pepun Barua <pepunbarua@gmail.com>
2023-10-10 18:41:42 +0600

    Create wp-content_location_modifiation.php

---
+++ WordPress/wp-content_location_modifiation.php
@@ -0,0 +1,9 @@
+
+define ('WP_CONTENT_FOLDERNAME', 'assets');
+define ('WP_CONTENT_DIR', ABSPATH . WP_CONTENT_FOLDERNAME) ;
+
+define('WP_SITEURL', 'https://' . $_SERVER['HTTP_HOST'] . '/');
+define('WP_CONTENT_URL', WP_SITEURL . WP_CONTENT_FOLDERNAME);
+define( 'WP_HOME', 'https://' . $_SERVER['HTTP_HOST'] . '/' );
+
+define( 'UPLOADS', 'files' );
