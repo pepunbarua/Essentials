e2ae37b2d4e84014db5414d335751d9bea5f436c
Pepun Barua <pepunbarua@gmail.com>
2023-10-10 19:14:58 +0600

    Update wp-content_location_modifiation.php

--- WordPress/wp-content_location_modifiation.php
+++ WordPress/wp-content_location_modifiation.php
@@ -1,8 +1,13 @@
+<?php
+//এই কোড দিয়ে wp-content ফোল্ডার এর নাম পরিবর্তন করে, অর্থাৎ আপনি এখন ফোল্ডার রিনেম করে assets নাম দিতে পারেন।
 define ('WP_CONTENT_FOLDERNAME', 'assets');
 define ('WP_CONTENT_DIR', ABSPATH . WP_CONTENT_FOLDERNAME) ;
+
+//এই কোড দিয়ে uploads ফোল্ডার এর নাম পরিবর্তন করে, অর্থাৎ আপনি এখন ফোল্ডার রিনেম করে files নাম দিতে পারেন। তবে এক্ষেত্র লক্ষনীয় বিষয় হলো
+define( 'UPLOADS', 'files' );
 
 define('WP_SITEURL', 'https://' . $_SERVER['HTTP_HOST'] . '/');
 define('WP_CONTENT_URL', WP_SITEURL . WP_CONTENT_FOLDERNAME);
 define( 'WP_HOME', 'https://' . $_SERVER['HTTP_HOST'] . '/' );
 
-define( 'UPLOADS', 'files' );
+
