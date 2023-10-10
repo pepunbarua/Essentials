746d98619bb22bb40ba39d880990c531e0e9369e
Pepun Barua <pepunbarua@gmail.com>
2023-10-10 19:25:22 +0600

    Update wp-content_location_modifiation.php

--- WordPress/wp-content_location_modifiation.php
+++ WordPress/wp-content_location_modifiation.php
@@ -3,7 +3,12 @@
 define ('WP_CONTENT_FOLDERNAME', 'assets');
 define ('WP_CONTENT_DIR', ABSPATH . WP_CONTENT_FOLDERNAME) ;
 
-//এই কোড দিয়ে uploads ফোল্ডার এর নাম পরিবর্তন করে, অর্থাৎ আপনি এখন ফোল্ডার রিনেম করে files নাম দিতে পারেন। তবে এক্ষেত্র লক্ষনীয় বিষয় হলো
+/* এই কোড দিয়ে uploads ফোল্ডার এর নাম পরিবর্তন করে, অর্থাৎ আপনি এখন ফোল্ডার রিনেম করে files নাম দিতে পারেন।
+তবে এক্ষেত্র লক্ষনীয় বিষয় হলো, আমরা uploads এর নাম পরিবর্তন করেছি তবে সেটাকে রুট ফোল্ডারে রেখেছ, অর্থাৎ নিচের কোডটি ব‍্যবহার করলে
+অবশ‍্যই upload ফোল্ডারটি wp-content থেকে বের করে রুট ফোল্ডারে নিয়ে আসতে হবে।
+অথবা আপনি define( 'UPLOADS', 'assets/files'); এভাবে ব‍্যবহার করতে পারেন,
+তাহলে upload ফোল্ডারটি আর wp-content/assets থেকে বের করতে হবে না শুধুমাত্র uploads ফোল্ডারকে রিনেম করে files লিখলেই চলবে।
+*/
 define( 'UPLOADS', 'files' );
 
 define('WP_SITEURL', 'https://' . $_SERVER['HTTP_HOST'] . '/');
